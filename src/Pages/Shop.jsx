78bc4e84a2473819d4f3e7235a535d179f8e43a6
Yasin Yagmur <yasin.yagmur@gmail.com>
2024-10-15 21:46:50 +0200

    hero jsx ve css oluşturuldu

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "../Components/Hero/Hero";
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <div>
+      <Hero />
+    </div>
+  );
+};
 
-export default Shop
+export default Shop;
