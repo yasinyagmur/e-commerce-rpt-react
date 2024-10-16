e765203a2b93880da39b7601e9779cf612d8db90
Yasin Yagmur <yasin.yagmur@gmail.com>
2024-10-16 22:03:51 +0200

    offers component created

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,10 +5,10 @@ const Item = (props) => {
   return (
     <div className="item">
       <img src={props.image} alt="" />
-      <p>{props.name}</p>
+      <p>${props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{props.new_price} </div>
-        <div className="item-price-new">{props.old_price} </div>
+        <div className="item-price-new">${props.new_price} </div>
+        <div className="item-price-new">${props.old_price} </div>
       </div>
     </div>
   );
