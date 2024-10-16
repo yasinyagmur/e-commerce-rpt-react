3c0dd75feee69548e665d8334dc56b2721c350c7
Yasin Yagmur <yasin.yagmur@gmail.com>
2024-10-16 21:44:03 +0200

    popular component created

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Item.css";
-const Item = () => {
-  return <div className="item"></div>;
+
+const Item = (props) => {
+  return (
+    <div className="item">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">{props.new_price} </div>
+        <div className="item-price-new">{props.old_price} </div>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
